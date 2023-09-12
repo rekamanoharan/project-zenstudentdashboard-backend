3ca3e19f17d089085358d9c144d2053efa55118b
rekamanoharan <rekamano15@gmail.com>
2023-09-12 19:52:00 +0530

    zenclone-backend

--- server.js
+++ server.js
@@ -19,11 +19,11 @@ app.use(cors(corsOptions));
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
   res.header(
-    "Access-Control-Allow-Headers",
+    "Access-Control-Allow-Headers:*",
     "Origin, X-Requested-With, Content-Type, Accept"
   );
   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
-  res.header("Access-Control-Allow-Credentials", "true");
+  // res.header("Access-Control-Allow-Credentials", "true");
   next();
 });
 
